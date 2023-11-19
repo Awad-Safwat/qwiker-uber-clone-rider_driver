42a3cc5594101413a00f1a26b9f1e7ec7bcbff50
Awad-Safwat <awad.safwat1999@gmail.com>
2023-11-19 17:19:51 +0200

    connecte to github

---
+++ qwiker_driver/android/app/src/main/kotlin/com/example/qwiker_driver/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.qwiker_driver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ qwiker_rider/android/app/src/main/kotlin/com/example/qwiker_rider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.qwiker_rider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
