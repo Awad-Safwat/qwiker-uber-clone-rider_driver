c12beb0c4fad0a9fc1ccb91cb4d42d2db916a237
Awad-Safwat <awad.safwat1999@gmail.com>
2023-11-19 17:29:08 +0200

    dummy commit testing

--- qwiker_rider/lib/main.dart
+++ qwiker_rider/lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
