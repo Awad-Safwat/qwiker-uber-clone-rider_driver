4aa734ed58555261bc47b03c947b3664c9d87f61
Awad-Safwat <awad.safwat1999@gmail.com>
2023-12-07 16:18:23 +0200

    added onboarding to Driver app

---
+++ qwiker_driver/lib/core/theaming/app_fonts.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppFonts {
+  static const TextStyle medel_36 = TextStyle(
+    fontSize: 36,
+    fontFamily: 'Medel',
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle medel_28 = TextStyle(
+    fontSize: 28,
+    fontFamily: 'Medel',
+    fontWeight: FontWeight.normal,
+  );
+
+  static TextStyle poppinsMedium_20 = GoogleFonts.poppins(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle poppinsMedium_16 = GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: const Color(0xff525252),
+  );
+  static TextStyle poppinsMedium_18 = GoogleFonts.poppins(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+}
---
+++ qwiker_rider/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:qwiker_rider/rider_app.dart';
+
+void main() {
+  runApp(const RiderApp());
+}
